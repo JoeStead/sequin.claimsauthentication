81261dbc56939c137ff0508067b0d172eb5dbd55
Jason Mitchell <jasonmitchell@live.co.uk>
2016-01-19 19:59:19 +0000

    Copying code from main repo

---
+++ samples/Sample.Web.ClaimsAuthentication/Commands/MyAuthenticatedCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Sample.Web.ClaimsAuthentication.Commands
+{
+    using System.Threading.Tasks;
+    using Sequin.Core;
+
+    public class MyAuthenticatedCommandHandler : IHandler<MyAuthenticatedCommand>
+    {
+        public Task Handle(MyAuthenticatedCommand command)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
---
+++ tests/Sequin.ClaimsAuthentication.Integration/Fakes/AnonymousCommandHandler.cs
@@ -0,0 +1,13 @@
+﻿namespace Sequin.ClaimsAuthentication.Integration.Fakes
+{
+    using System.Threading.Tasks;
+    using Sequin.Core;
+
+    public class AnonymousCommandHandler : IHandler<AnonymousCommand>
+    {
+        public Task Handle(AnonymousCommand command)
+        {
+            return Task.FromResult(0);
+        }
+    }
+}
