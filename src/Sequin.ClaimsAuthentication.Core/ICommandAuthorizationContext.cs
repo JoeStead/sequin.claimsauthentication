475a318fcd3561aa88998112d4580d9d5f53ec3c
Jason Mitchell <jasonmitchell@live.co.uk>
2016-01-19 20:16:56 +0000

    Adding missing project

---
+++ src/Sequin.ClaimsAuthentication.Core/ICommandAuthorizationContext.cs
@@ -0,0 +1,11 @@
+﻿namespace Sequin.ClaimsAuthentication.Core
+{
+    public interface ICommandAuthorizationContext
+    {
+        void Reject();
+        bool HasClaim(string type, string value);
+
+        bool IsAuthenticated { get; }
+        bool IsAuthorized { get; }
+    }
+}
